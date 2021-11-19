9e41b026d360c908ded52598fa1ff5b958777ac7
Your Name <you@example.com>
2021-11-19 22:28:43 +0600

    user Registration & signin Form included

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import Footer from './Footer';
-import Header from './Header';
+// import Footer from './Footer';
+// import Header from './Header';
+import Register from './UserRegister';
+import SignIn from './UserSignIn';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Footer />
+      {/* <Header /> */}
+      <Register/>
+      {/* <SignIn/> */}
+      {/* <Footer /> */}
     </div>
   );
 }
