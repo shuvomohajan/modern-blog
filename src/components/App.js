2ed20c8ca63432bcb5daaebecf3735a2e3cda669
skdhar50 <skdhar50@gmail.com>
2021-11-26 14:45:27 +0600

    responsiveness added to login and registration page.

--- src/components/App.js
+++ src/components/App.js
@@ -2,32 +2,54 @@
 
 import Login from "./auth/Login";
 import Register from "./auth/Register";
-import Navbar from "./website/includes/Navbar";
 import Dashboard from "./website/Dashboard";
 import Profile from "./website/Profile";
 import { Routes, Route } from "react-router-dom";
 import Editor from "../components/website/Editor/Editor";
 import PostDetails from "../components/website/Posts/PostDetails";
-import Comments from "./website/Comments/Comments";
+import WithNav from "./website/includes/WithNav";
 
 function App() {
-  return (
-    <div className="bg-gray-100 h-screen max-h-full overflow-scroll">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/profile/:userId" element={<Profile />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/registration" element={<Register />} />
-        <Route path="/editor" element={<Editor />} />
-        <Route path="/post-details/:postId" element={<PostDetails />}>
-          <Route path="comments" element={<Comments />} />
-        </Route>
-      </Routes>
-    </div>
-  );
-
+	return (
+		<div className="bg-gray-100 h-screen max-h-full overflow-scroll">
+			<Routes>
+				<Route path="/login" element={<Login />} />
+				<Route path="/registration" element={<Register />} />
+				<Route
+					path="/"
+					element={
+						<WithNav>
+							<Dashboard />
+						</WithNav>
+					}
+				/>
+				<Route
+					path="/profile/:userId"
+					element={
+						<WithNav>
+							<Profile />
+						</WithNav>
+					}
+				/>
+				<Route
+					path="/editor"
+					element={
+						<WithNav>
+							<Editor />
+						</WithNav>
+					}
+				/>
+				<Route
+					path="/post-details/:postId"
+					element={
+						<WithNav>
+							<PostDetails />
+						</WithNav>
+					}
+				/>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
-
