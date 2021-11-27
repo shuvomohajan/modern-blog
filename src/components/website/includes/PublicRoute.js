762ed4064f88ac6c20bdf7ae27fced65036ba528
skdhar50 <skdhar50@gmail.com>
2021-11-28 00:31:46 +0600

    private and public route added.

---
+++ src/components/website/includes/PublicRoute.js
@@ -0,0 +1,14 @@
+import {useAuth} from "../../../context/AuthContext";
+import {Navigate} from "react-router-dom";
+
+export function PrivateRoute({children}) {
+    const {token} = useAuth();
+
+    return token ? children : <Navigate to="/login" />;
+}
+
+export function PublicRoute({children}) {
+    const {token} = useAuth();
+
+    return token ?  <Navigate to="/" /> : children;
+}
