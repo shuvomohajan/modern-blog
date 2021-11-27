bcfb7e5f1d06cdd43849f848b3e8238399a0406f
skdhar50 <skdhar50@gmail.com>
2021-11-28 00:36:56 +0600

    simple design changed.

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,66 +1,67 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const AuthContext = React.createContext();
 
 export function useAuth() {
-    return useContext(AuthContext);
+	return useContext(AuthContext);
 }
 
-export function AuthProvider({children}) {
-    const [error, setError] = useState(false);
+export function AuthProvider({ children }) {
+	const [error, setError] = useState(false);
 	const [loading, setLoading] = useState(false);
 	const [token, setToken] = useState("");
 	const url = "http://modern-blog-backend.test/api/login";
 	const navigate = useNavigate();
 
-    useEffect(() => {
-        const localToken = window.localStorage.getItem("token");
-        setToken(localToken);
-    }, [token, setToken])
+	useEffect(() => {
+		const localToken = window.localStorage.getItem("token");
+		setToken(localToken);
+	}, [token, setToken]);
 
-    // login
-    function login(email, password) {
-        axios.post(url, {
-			email: email,
-			password: password
-		})
-        .then(res => {
-            setError(false);
-            setLoading(false);
-            setToken(res.data.token);
-            window.localStorage.setItem('token', res.data.token);
+	// login
+	function login(email, password) {
+		axios
+			.post(url, {
+				email: email,
+				password: password,
+			})
+			.then((res) => {
+				setError(false);
+				setLoading(false);
+				setToken(res.data.token);
+				window.localStorage.setItem("token", res.data.token);
 
-            navigate("/", {
-				replace: true
+				navigate("/", {
+					replace: true,
+				});
+			})
+			.catch((error) => {
+				setError(error);
 			});
-        })
-        .catch(error => {
-			setError(error);
+	}
+
+	// logout
+	function logout() {
+		localStorage.removeItem("token");
+		setToken("");
+
+		navigate("/", {
+			replace: true,
 		});
-    }
+	}
 
-    // logout
-    function logout() {
-        localStorage.removeItem('token');
-        setToken("");
+	const value = {
+		token,
+		error,
+		login,
+		logout,
+	};
 
-        navigate("/", {
-            replace: true
-        })
-    }
-
-    const value = {
-        token,
-        error,
-        login,
-        logout
-    };
-
-    return (
-        <AuthContext.Provider value={value}>
-            {!loading && children}
-        </AuthContext.Provider>
-    );
+	return (
+		<AuthContext.Provider value={value}>
+			{!loading && children}
+		</AuthContext.Provider>
+	);
 }
